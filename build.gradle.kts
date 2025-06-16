ef9381ce0c4bf1de9d430221163cea98bc8321f1
freeze-dolphin <freeze-dolphin@outlook.com>
2025-06-16 14:49:38 +0800

    build: disable native build on default, making it accessible on jitpack.io

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,23 +59,24 @@ version = getCheckedOutGitCommitHash()
 kotlin {
     jvm()
 
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
-        binaries {
-            sharedLib {
-                baseName = "${rootProject.name}-native_${project.version}"
-                version = project.version.toString()
+    if (!System.getenv().getOrElse("BUILD_NATIVE") { null }.isNullOrEmpty()) {
+        val hostOs = System.getProperty("os.name")
+        val isArm64 = System.getProperty("os.arch") == "aarch64"
+        val isMingwX64 = hostOs.startsWith("Windows")
+        val nativeTarget = when {
+            hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+            hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+            hostOs == "Linux" && isArm64 -> linuxArm64("native")
+            hostOs == "Linux" && !isArm64 -> linuxX64("native")
+            isMingwX64 -> mingwX64("native")
+            else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        }
+        nativeTarget.apply {
+            binaries {
+                sharedLib {
+                    baseName = "${rootProject.name}-native_${project.version}"
+                    version = project.version.toString()
+                }
             }
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "AffCompose"
+rootProject.name = "aff-compose"
